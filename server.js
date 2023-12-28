b679a04f36daceac00dc7f587e8c87cde3b624cb
Abdul Rehman <arrehman0r@gmail.com>
2023-12-28 20:39:50 +0500

    final commit

--- server.js
+++ server.js
@@ -16,46 +16,61 @@ app.get("/", (req, res) => {
 });
 
 // Handle form submission and send confirmation email
-// Handle form submission and send confirmation email
-app.post("/send-email", (req, res) => {
+app.post("/send-email", async (req, res) => {
   const { name, email, message } = req.body;
 
-  // Log form data for debugging
-  console.log("Form Data from server:", { name, email, message });
-
-  // Create a Nodemailer transporter
-  const transporter = nodemailer.createTransport({
-    service: "gmail",
-    auth: {
-      user: "arrehman1g@gmail.com", // Your Gmail email address
-      pass: "afsd1423", // Your Gmail password or an app-specific password
-    },
-  });
-
-  // Check if email is provided
-  if (!email) {
-    console.error("No email address provided");
-    return res.status(400).json({ message: "No email address provided" });
-  }
-
-  // Email content
-  let mailOptions = {
-    from: "arrehman1g@gmail.com",
-    to: email,
-    subject: "Confirmation Email",
-    text: `Hello ${name},\n\nThank you for your message. We have received your inquiry and will get back to you soon.\n\nBest regards,\nThe Team`,
-  };
-
-  // Send email
-  transporter.sendMail(mailOptions, (error, info) => {
-    if (error) {
-      console.error("Error sending email yessss:", error);
-      res.status(500).json({ message: "Error sending email yess" });
-    } else {
-      console.log("Email sent:", info.response);
-      res.json({ message: "Form submitted successfully" });
+  try {
+    // Validate form data
+    if (!name || !email || !message) {
+      throw new Error("Invalid form data");
     }
-  });
+
+    // Create a Nodemailer transporter for Gmail
+    const transporter = nodemailer.createTransport({
+      service: "gmail",
+      auth: {
+        user: "enter your emial", // Your Gmail email address
+        pass: "enter your password here", // Your Gmail password or an app-specific password
+      },
+    });
+
+    // Add event listener for 'login' event
+    transporter.on("login", (auth) => {
+      console.log("Login successful:", auth);
+    });
+
+    // Add event listener for 'error' event
+    transporter.on("error", (error) => {
+      console.error("Error during login:", error);
+      res.status(500).json({ message: "Error during login" });
+    });
+
+    // Check if email is provided
+    if (!email) {
+      console.error("No email address provided");
+      return res.status(400).json({ message: "No email address provided" });
+    }
+
+    // Email content
+    let mailOptions = {
+      from: "emial here", //enter emial here too
+      to: email,
+      subject: "Confirmation Email",
+      text: `Hello ${name},\n\nThank you for your message. We have received your inquiry and will get back to you soon.\n\nBest regards,\nThe Team`,
+    };
+
+    // Send email
+    const info = await transporter.sendMail(mailOptions);
+    console.log("Email sent:", info.response);
+
+    // Send a success response
+    res.json({ message: "Form submitted successfully" });
+  } catch (error) {
+    console.error("Error:", error);
+
+    // Send an error response
+    res.status(500).json({ message: "Internal Server Error" });
+  }
 });
 
 app.listen(port, () => {
